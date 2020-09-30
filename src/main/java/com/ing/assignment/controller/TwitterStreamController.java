22e20192f241b8c1f6d57236b51130212256b7a6
Jagratihooda <jagratigit@gmail.com>
2020-09-30 19:14:49 +0200

    Sonar Fixes

--- src/main/java/com/ing/assignment/controller/TwitterStreamController.java
+++ src/main/java/com/ing/assignment/controller/TwitterStreamController.java
@@ -1,9 +1,12 @@
 package com.ing.assignment.controller;
+
 import com.ing.assignment.model.Tweet;
 import com.ing.assignment.service.TweetStreamProcessService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,20 +17,18 @@ import java.util.List;
 
 /**
  * @author Jagrati
- *This rest controller is for Reading and Storing Tweets.
+ * This rest controller is for Reading and Storing Tweets.
  */
 @RestController
 @RequestMapping("/tweetstreaming/")
 public class TwitterStreamController {
-	private static final Logger LOGGER = LoggerFactory.getLogger(TwitterStreamController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterStreamController.class);
 
-	@Autowired
-	private TweetStreamProcessService tweetProcessService;
+    @Autowired
+    private TweetStreamProcessService tweetProcessService;
 
-	@GetMapping(path = "process-tweets")
-	public List<Tweet>   readTweets() throws IOException, AuthenticationException {
-		List<Tweet> tweetList = tweetProcessService.processTweets();
-		LOGGER.info("Tweets are dumped on log file");
-		return tweetList;
-	}
+    @GetMapping(path = "process-tweets")
+    public List<Tweet> readTweets() throws IOException {
+        return tweetProcessService.processTweets();
+    }
 }
