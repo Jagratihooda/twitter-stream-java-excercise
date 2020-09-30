db484e49bd8604fb2f63b3cbef03998469c2c0ee
Jagratihooda <jagratigit@gmail.com>
2020-09-30 19:24:14 +0200

    Spinning image addition

--- src/main/resources/static/js/tweet.js
+++ src/main/resources/static/js/tweet.js
@@ -1,17 +1,22 @@
 angular.module('tweetApp', [])
-.controller('tweet', function($scope, $http) {
-	$scope.warnVar=true;
-	$scope.tableVar=true;
-	var req = {
-			method: 'POST',
+	.controller('tweet', function($scope, $http) {
+		$scope.loadingVar=true;
+		$scope.warnVar=false;
+		$scope.tableVar=false;
+		$scope.preLoadVal=false;	var req = {
+			method: 'GET',
 			url: 'http://localhost:8081/tweetstreaming/process-tweets',
 	}
 	$http(req).
 	then(function mySuccess(response) {
+		$scope.preLoadVal=true;
+		$scope.loadingVar=false;
 		$scope.warnVar=false;
 		$scope.tableVar=true;
 		$scope.tweets = response.data;
 	},function myError(response) {
+		$scope.preLoadVal=true;
+		$scope.loadingVar=false;
 		$scope.warnVar=true;
 		$scope.tableVar=false;
 	});
